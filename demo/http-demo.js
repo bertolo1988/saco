f7778a2882833eb00e18c31162a3d3fa765f7df1
bertolo1988 <tiagobertolo@gmail.com>
2017-03-24 00:56:17 +0000

    moved demos to demo folder

---
+++ demo/http-demo.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const angularCliDemoPath = path.join(__dirname, '../examples/angular-cli');
+
+let Saco = require('../dist/src/Server');
+new Saco.Server({ folder: angularCliDemoPath, port: 3022, verbose: true }).start();
---
+++ demo/https-demo.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const angularCliDemoPath = path.join(__dirname, '../examples/angular-cli');
+const key = path.join(__dirname, '../examples/certificates/alice.key');
+const cert = path.join(__dirname, '../examples/certificates/alice.crt');
+
+let Saco = require('../dist/src/Server');
+new Saco.Server({ folder: angularCliDemoPath, port: 3022, verbose: true, key, cert }).start();
